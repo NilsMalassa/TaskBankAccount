307587d0d8d92b6bdf1cc9b1b7db5ec12d08273e
DeathUser <nilsmalassa0@gmail.com>
2026-08-21 19:47:42 +0200

    init added AccountHolder.cs

---
+++ AccountHolder.cs
@@ -0,0 +1,38 @@
+﻿namespace DefaultNamespace
+{
+    public class AccountHolder
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        //public List<Bankaccount> Bankaccounts;
+
+        private AccountHolder(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public static AccountHolder? TryCreateNewAccountHolder(string surname, string name, int age,
+            out string errorMessage)
+        {
+            if (age < 18)
+            {
+                errorMessage = "You are not old enough for Registration.";
+                return null;
+            }
+
+            errorMessage = string.Empty;
+            return new AccountHolder(name, surname, age);
+        }
+
+        public void ShowOwnBankAccounts()
+        {
+        }
+
+        public void AddNewAccount()
+        {
+        }
+    }
+}
