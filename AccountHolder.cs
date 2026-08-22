7652994a765ea65318de19d07702d56faa353e67
DeathUser <nilsmalassa0@gmail.com>
2026-08-22 21:27:21 +0200

    added Bankaccount.cs and modified methods in AccountHolder.cs

--- AccountHolder.cs
+++ AccountHolder.cs
@@ -1,17 +1,18 @@
-﻿namespace DefaultNamespace
+﻿namespace TaskBankAccount
 {
     public class AccountHolder
     {
-        public string Name { get; private set; }
+        public string Name { get; }
         public string Surname { get; private set; }
-        public int Age { get; private set; }
-        //public List<Bankaccount> Bankaccounts;
+        public int Age { get; }
+        readonly List<Bankaccount>_bankaccounts;
 
         private AccountHolder(string name, string surname, int age)
         {
             Name = name;
             Surname = surname;
             Age = age;
+            _bankaccounts = new();
         }
 
         public static AccountHolder? TryCreateNewAccountHolder(string surname, string name, int age,
@@ -29,10 +30,22 @@
 
         public void ShowOwnBankAccounts()
         {
+            foreach (var bankAccount in _bankaccounts)
+            {
+                bankAccount.ShowAccountInfo();
+            }
         }
 
-        public void AddNewAccount()
+        public void AddNewAccount(Bankaccount newAccount)
         {
+            if (_bankaccounts.Contains(newAccount))
+            {
+                Console.WriteLine("Account already exists.");
+                return;
+            }
+            
+            _bankaccounts.Add(newAccount);
+            Console.WriteLine("New account added.");
         }
     }
 }
